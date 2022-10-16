2eef0dbd1f03e0dbc4a1db1768db96818c418a6f
tumpa02 <jtumpa@gmail.com>
2022-10-16 22:17:58 +0200

    init

--- example/src/App.js
+++ example/src/App.js
@@ -1,10 +1,10 @@
 import React from 'react'
 
-import { ExampleComponent } from 'omniform'
+import { Form } from 'omniform'
 import 'omniform/dist/index.css'
 
 const App = () => {
-  return <ExampleComponent text="Create React Library Example 😄" />
+  return <Form text="Create React Library Example 😄" />
 }
 
 export default App
--- src/index.js
+++ src/index.js
@@ -1,6 +1,29 @@
 import React from 'react'
 import styles from './styles.module.css'
 
-export const ExampleComponent = ({ text }) => {
-  return <div className={styles.test}>Example Component: {text}</div>
+import { useForm } from 'react-hook-form'
+
+export const Form = ({ text }) => {
+  const {
+    register,
+    handleSubmit,
+    watch,
+    formState: { errors }
+  } = useForm()
+
+  const onSubmit = (data) => {
+    window.alert(JSON.stringify(data))
+  }
+
+  console.log(watch('example'))
+
+  return (
+    <div className={styles.test}>
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <input {...register('example', { required: true })} />
+        {errors.example && <span>This is required.</span>}
+        <input type='submit' />
+      </form>
+    </div>
+  )
 }
